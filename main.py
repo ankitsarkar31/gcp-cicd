2a7002f8e7e1c9ee0e199828609615807fc81cda
Ankit Sarkar <92983704+ankitsarkar31@users.noreply.github.com>
2025-07-11 14:25:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def return_hostname():
-    return "This is an example wsgi app served from {} to {}".format(socket.gethostname())
+    return "This webpage is running in pod:{}".format(socket.gethostname())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
